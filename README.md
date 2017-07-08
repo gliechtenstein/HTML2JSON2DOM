4cd6ca79e960b8e7b4377de7d976578967611acb
gliechtenstein <ethan.gliechtenstein@gmail.com>
2017-07-08 16:39:18 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# HTML to Cell
+
+A notebook that turns HTML => Cell => DOM Edit
+Add topics
+
+# Built With
+
+1. cell: https://github.com/intercellular/cell
+2. to_cell: https://github.com/devsnek/to_cell
+3. codemirror: https://github.com/codemirror/CodeMirror
+
+# How it works
+
+There are 3 columns
+
+*             | Column 1 | Column 2 | Column3
+--------------|----------|----------|-----------
+What it does  | enter HTML | turn HTML from column 1 into Cell | turn cell from column 2 into DOM
---
+++ editor.js
@@ -0,0 +1,31 @@
+CM = function(gene) { 
+  return {
+    $type: "textarea",
+    id: gene.id ? gene.id : null,
+    _ed: null,
+    _out: gene._out,
+    _in: function(message) {
+      if(this._ed) this._ed.setValue(message);
+    },
+    _default: {
+      mode: "xml", htmlMode: true, lineNumbers: true, lineWrapping: true, lint: true, styleActiveLine: true, autoCloseBrackets: true, matchBrackets: true, viewportMargin: true, theme: "neo", gutters: ["CodeMirror-lint-markers"]
+    },
+    $init: function(){
+      var self = this;
+      for(var key in gene.options) {
+        this._default[key] = gene.options[key];
+      }
+      this._ed = CodeMirror.fromTextArea(this, this._default);
+      this._wrapper = this._ed.getWrapperElement();
+      for(var key in gene.style) {
+        this._wrapper.style[key] = gene.style[key];
+      }
+      try{
+        this._ed.on("change", function(e){
+          if(self._out) self._out(self._ed.getValue())
+        })
+      } catch (e) {}
+      if(self._out) self._out(self._ed.getValue())
+    }
+  }
+}
